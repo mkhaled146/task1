5b67c9ad4b1aa5a80e9a27f684cdce2aa5ca016a
Your Name <you@example.com>
2020-10-22 18:00:20 +0300

    add new python file

---
+++ task 1.py
@@ -0,0 +1,52 @@
+
+Python Variables Tasks
+
+Define a Variable of type bool
+Answer :( true . false )
+
+
+whate is the benfit of type method?
+Answer :usefule to know how to use variable
+
+
+can we use semi colon ; with python ?
+Answer : yes , but littel
+
+
+pyhon is interpreted or compiled ?
+Answer : interpreter
+
+
+What is the differences between low level & high level ?
+Answer : 
+ 
+ low level computer language 
+used 0,1
+easy to be executed
+hard to program 
+c , assembly
+
+ high level computer language 
+takes less time
+easy to be corrected
+easy to program 
+c# , python
+
+
+
+Python Operators Tasks
+
+What is the differences between = , == ?
+ =  a assignment operator  mean equal
+ == a comparison operator  mean question if it equal
+
+
+What do we mean by using != ?
+ !=  mean not equal
+
+
+What is the operator precedence ?
+()
+**
+* , /
++ , -
